746dd268badfbef6d4f0f5a3e831379def5d0f26
Alexandr Bochev <abochev@gmail.com>
2022-10-20 16:38:52 +0300

    tablet

--- components/UsersItem.tsx
+++ components/UsersItem.tsx
@@ -4,14 +4,16 @@ const UsersItem = () => {
   return (
     <>
     <div className={styles.users_item}>
-      <div className={styles.grid}>
+      <div className={styles.grid1}>
         <div className={styles.face}></div>
       </div>
-      <div className={styles.grid}>Mr. smith</div>
-      <div className={styles.grid}>offline</div>
-      <div className={styles.grid}>Toronto</div>
-      <div className={styles.grid}>35 year</div>
-      <div className={styles.grid}>action</div>
+      <div className={styles.grid2}>Mr. smith</div>
+      <div className={styles.grid3}>offline</div>
+      <div className={styles.grid4}>Toronto</div>
+      <div className={styles.grid5}>35 year</div>
+      <div className={styles.grid6}>
+        <div className={styles.btn}>Chat</div>
+      </div>
     </div>
     </>
   )
